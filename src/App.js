d7bdb3dcaa659e4a2000334ed0d2004e82a1ee7b
Adeleke-emmanuel <135076483+Adeleke-emmanuel@users.noreply.github.com>
2023-11-23 22:21:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './components/Main';
+import './Components/style.css';
+import Card from './components/card';
+function App(){
+    return {
+        <>
+            <Main/>
+        </>
+    };
+}
+
+export default App;
